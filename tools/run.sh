5211640882630e47a05b9fd600fe79605739229d
hroost <hroostik@gmail.com>
2023-07-21 19:42:04 +0200

    fix: typo

--- tools/run.sh
+++ tools/run.sh
@@ -8,8 +8,8 @@ curl -sL "https://mask-api.icloud.com/egress-ip-ranges.csv" | cut -d ',' -f 1 > 
   wc -l ipv4-only.txt && \
   wc -l ipv6-only.txt && \
   cidr-merger -eo ip-ranges.txt egress-ip-ranges.txt && \
-  cidr-merger -eo ip-ranges.txt ipv4-only.txt && \
-  cidr-merger -eo ip-ranges.txt ipv6-only.txt && \
+  cidr-merger -eo ipv4-ranges.txt ipv4-only.txt && \
+  cidr-merger -eo ipv6-ranges.txt ipv6-only.txt && \
   wc -l ip-ranges.txt && \
   wc -l ipv4-ranges.txt && \
   wc -l ipv6-ranges.txt
