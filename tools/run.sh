4475ceeec8f27fd06b5d276dc8b59fdd8608d177
hroost <hroostik@gmail.com>
2023-07-21 20:17:22 +0200

    add ipset format

--- tools/run.sh
+++ tools/run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -x
 
+## build basics
 curl -sL "https://mask-api.icloud.com/egress-ip-ranges.csv" | cut -d ',' -f 1 > egress-ip-ranges.txt && \
   grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+(/[0-9]+)?$' egress-ip-ranges.txt > ipv4-only.txt && \
   grep -E '^[0-9a-fA-F:]+(/[0-9]+)?$' egress-ip-ranges.txt > ipv6-only.txt && \
@@ -13,3 +14,20 @@ curl -sL "https://mask-api.icloud.com/egress-ip-ranges.csv" | cut -d ',' -f 1 > 
   wc -l ip-ranges.txt && \
   wc -l ipv4-ranges.txt && \
   wc -l ipv6-ranges.txt
+
+
+## ipset list
+echo "create icloudrelay hash:net" > ip-ranges.ipset.txt
+for ip in $(cat ip-ranges.txt); do
+  echo "add icloudrelay $ip" >> ip-ranges.ipset.txt
+done
+
+echo "create icloudrelayipv4 hash:net" > ipv4-ranges.ipset.txt
+for ip in $(cat ipv4-ranges.txt); do
+  echo "add icloudrelayipv4 $ip" >> ipv4-ranges.ipset.txt
+done
+
+echo "create icloudrelayipv6 hash:net" > ipv6-ranges.ipset.txt
+for ip in $(cat ipv6-ranges.txt); do
+  echo "add icloudrelayipv6 $ip" >> ipv6-ranges.ipset.txt
+done
