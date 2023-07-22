95e8fc25c6ed124b3058a436ca21965bafb47528
hroost <hroostik@gmail.com>
2023-07-22 21:45:52 +0200

    move separate lists into subdirs

--- tools/run.sh
+++ tools/run.sh
@@ -9,30 +9,30 @@ curl -sL "https://mask-api.icloud.com/egress-ip-ranges.csv" | cut -d ',' -f 1 > 
   wc -l ipv4-only.txt && \
   wc -l ipv6-only.txt && \
   cidr-merger -eo ip-ranges.txt egress-ip-ranges.txt && \
-  cidr-merger -eo ipv4-ranges.txt ipv4-only.txt && \
-  cidr-merger -eo ipv6-ranges.txt ipv6-only.txt && \
+  cidr-merger -eo ipv4/ipv4-ranges.txt ipv4-only.txt && \
+  cidr-merger -eo ipv6/ipv6-ranges.txt ipv6-only.txt && \
   wc -l ip-ranges.txt && \
-  wc -l ipv4-ranges.txt && \
-  wc -l ipv6-ranges.txt
+  wc -l ipv4/ipv4-ranges.txt && \
+  wc -l ipv6/ipv6-ranges.txt
 
 
 ## ipset list
-echo "create icloudrelay hash:net" > ip-ranges.ipset.txt
+echo "create icloudrelay hash:net" > ip-ranges.ipset
 for ip in $(cat ip-ranges.txt); do
-  echo "add icloudrelay $ip" >> ip-ranges.ipset.txt
+  echo "add icloudrelay $ip" >> ip-ranges.ipset
 done
 
-echo "create icloudrelayipv4 hash:net" > ipv4-ranges.ipset.txt
-for ip in $(cat ipv4-ranges.txt); do
-  echo "add icloudrelayipv4 $ip" >> ipv4-ranges.ipset.txt
+echo "create icloudrelayipv4 hash:net" > ipv4/ipv4-ranges.ipset
+for ip in $(cat ipv4/ipv4-ranges.txt); do
+  echo "add icloudrelayipv4 $ip" >> ipv4/ipv4-ranges.ipset
 done
 
-echo "create icloudrelayipv6 hash:net" > ipv6-ranges.ipset.txt
-for ip in $(cat ipv6-ranges.txt); do
-  echo "add icloudrelayipv6 $ip" >> ipv6-ranges.ipset.txt
+echo "create icloudrelayipv6 hash:net" > ipv6/ipv6-ranges.ipset
+for ip in $(cat ipv6/ipv6-ranges.txt); do
+  echo "add icloudrelayipv6 $ip" >> ipv6/ipv6-ranges.ipset
 done
 
 ## json files
 cat ip-ranges.txt | jq -R --slurp 'split("\n") | .[:-1]' > ip-ranges.json
-cat ipv4-ranges.txt | jq -R --slurp 'split("\n") | .[:-1]' > ipv4-ranges.json
-cat ipv6-ranges.txt | jq -R --slurp 'split("\n") | .[:-1]' > ipv6-ranges.json
+cat ipv4/ipv4-ranges.txt | jq -R --slurp 'split("\n") | .[:-1]' > ipv4/ipv4-ranges.json
+cat ipv6/ipv6-ranges.txt | jq -R --slurp 'split("\n") | .[:-1]' > ipv6/ipv6-ranges.json
