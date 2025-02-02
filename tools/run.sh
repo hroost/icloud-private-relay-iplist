9298a0470b54ccb2818999c42713040ee546adbf
carlitros900 <carlitros900@gmail.com>
2025-02-02 10:30:49 +0100

    Segmented geolocalized information by country

--- tools/run.sh
+++ tools/run.sh
@@ -1,37 +1,57 @@
 #!/bin/sh
 
+doit() {
+  echo "Processing: $1"
+  mkdir -p $2ipv4 $2ipv6
+  [ -z "$1" ] && dash="" || { dash="-"; grep ",$1," egress-ip-ranges.csv > $2egress-ip-ranges.csv; }
+  cut -d ',' -f 1 $2egress-ip-ranges.csv > $2egress-ip-ranges.txt && \
+    grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+(/[0-9]+)?$' $2egress-ip-ranges.txt > $2ipv4-only.txt && \
+    grep -E '^[0-9a-fA-F:]+(/[0-9]+)?$' $2egress-ip-ranges.txt > $2ipv6-only.txt && \
+    wc -l $2egress-ip-ranges.txt && \
+    wc -l $2ipv4-only.txt && \
+    wc -l $2ipv6-only.txt && \
+    cidr-merger -eo $2ip-ranges.txt $2egress-ip-ranges.txt && \
+    cidr-merger -eo $2ipv4/$1${dash}ipv4-ranges.txt $2ipv4-only.txt && \
+    cidr-merger -eo $2ipv6/$1${dash}ipv6-ranges.txt $2ipv6-only.txt && \
+    wc -l $2ip-ranges.txt && \
+    wc -l $2ipv4/$1${dash}ipv4-ranges.txt && \
+    wc -l $2ipv6/$1${dash}ipv6-ranges.txt
+
+  ## ipset list
+  echo "create icloudrelay hash:net" > $2ip-ranges.ipset
+  for ip in $(cat $2ip-ranges.txt); do
+    echo "add icloudrelay $ip" >> $2ip-ranges.ipset
+  done
+
+  echo "create icloudrelayipv4 hash:net" > $2ipv4/$1${dash}ipv4-ranges.ipset
+  for ip in $(cat $2ipv4/$1${dash}ipv4-ranges.txt); do
+    echo "add icloudrelayipv4 $ip" >> $2ipv4/$1${dash}ipv4-ranges.ipset
+  done
+
+  echo "create icloudrelayipv6 hash:net" > $2ipv6/$1${dash}ipv6-ranges.ipset
+  for ip in $(cat $2ipv6/$1${dash}ipv6-ranges.txt); do
+    echo "add icloudrelayipv6 $ip" >> $2ipv6/$1${dash}ipv6-ranges.ipset
+  done
+
+  ## json files
+  cat $2ip-ranges.txt | jq -R --slurp 'split("\n") | .[:-1]' > $2ip-ranges.json
+  cat $2ipv4/$1${dash}ipv4-ranges.txt | jq -R --slurp 'split("\n") | .[:-1]' > $2ipv4/$1${dash}ipv4-ranges.json
+  cat $2ipv6/$1${dash}ipv6-ranges.txt | jq -R --slurp 'split("\n") | .[:-1]' > $2ipv6/$1${dash}ipv6-ranges.json
+}
+
 ## build basics
-curl -sL "https://mask-api.icloud.com/egress-ip-ranges.csv" | cut -d ',' -f 1 > egress-ip-ranges.txt && \
-  grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+(/[0-9]+)?$' egress-ip-ranges.txt > ipv4-only.txt && \
-  grep -E '^[0-9a-fA-F:]+(/[0-9]+)?$' egress-ip-ranges.txt > ipv6-only.txt && \
-  wc -l egress-ip-ranges.txt && \
-  wc -l ipv4-only.txt && \
-  wc -l ipv6-only.txt && \
-  cidr-merger -eo ip-ranges.txt egress-ip-ranges.txt && \
-  cidr-merger -eo ipv4/ipv4-ranges.txt ipv4-only.txt && \
-  cidr-merger -eo ipv6/ipv6-ranges.txt ipv6-only.txt && \
-  wc -l ip-ranges.txt && \
-  wc -l ipv4/ipv4-ranges.txt && \
-  wc -l ipv6/ipv6-ranges.txt
+curl -sLO "https://mask-api.icloud.com/egress-ip-ranges.csv"
 
+[ ! -e egress-ip-ranges.csv ] && echo "File does not exists egress-ip-ranges.csv" && exit 1
 
-## ipset list
-echo "create icloudrelay hash:net" > ip-ranges.ipset
-for ip in $(cat ip-ranges.txt); do
-  echo "add icloudrelay $ip" >> ip-ranges.ipset
-done
+cut -d ',' -f 2 egress-ip-ranges.csv | sort | uniq | sed '/^[[:space:]]*$/d' > egress-countries.txt
 
-echo "create icloudrelayipv4 hash:net" > ipv4/ipv4-ranges.ipset
-for ip in $(cat ipv4/ipv4-ranges.txt); do
-  echo "add icloudrelayipv4 $ip" >> ipv4/ipv4-ranges.ipset
-done
+# Do first the complete list, after do each country
+doit "" ""
+while IFS= read -r line; do
+    doit "$line" "geo_country/$line/$line-"
+done < egress-countries.txt
 
-echo "create icloudrelayipv6 hash:net" > ipv6/ipv6-ranges.ipset
-for ip in $(cat ipv6/ipv6-ranges.txt); do
-  echo "add icloudrelayipv6 $ip" >> ipv6/ipv6-ranges.ipset
-done
-
-## json files
-cat ip-ranges.txt | jq -R --slurp 'split("\n") | .[:-1]' > ip-ranges.json
-cat ipv4/ipv4-ranges.txt | jq -R --slurp 'split("\n") | .[:-1]' > ipv4/ipv4-ranges.json
-cat ipv6/ipv6-ranges.txt | jq -R --slurp 'split("\n") | .[:-1]' > ipv6/ipv6-ranges.json
+find . -name "*egress-ip-ranges.csv" -delete
+find . -name "*egress*.txt" -delete
+find . -name "*only.txt" -delete
