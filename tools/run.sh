63c3fe949b458e0711aa7c1589bf14b68649657b
Aleksei <amozgovenko@finst.com>
2026-05-04 16:13:48 +0200

    Harden update workflow and generator script

--- tools/run.sh
+++ tools/run.sh
@@ -1,37 +1,86 @@
 #!/bin/sh
 
-## build basics
-curl -sL "https://mask-api.icloud.com/egress-ip-ranges.csv" | cut -d ',' -f 1 > egress-ip-ranges.txt && \
-  grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+(/[0-9]+)?$' egress-ip-ranges.txt > ipv4-only.txt && \
-  grep -E '^[0-9a-fA-F:]+(/[0-9]+)?$' egress-ip-ranges.txt > ipv6-only.txt && \
-  wc -l egress-ip-ranges.txt && \
-  wc -l ipv4-only.txt && \
-  wc -l ipv6-only.txt && \
-  cidr-merger -eo ip-ranges.txt egress-ip-ranges.txt && \
-  cidr-merger -eo ipv4/ipv4-ranges.txt ipv4-only.txt && \
-  cidr-merger -eo ipv6/ipv6-ranges.txt ipv6-only.txt && \
-  wc -l ip-ranges.txt && \
-  wc -l ipv4/ipv4-ranges.txt && \
-  wc -l ipv6/ipv6-ranges.txt
+set -eu
 
+feed_url="https://mask-api.icloud.com/egress-ip-ranges.csv"
+tmpdir="$(mktemp -d)"
+trap 'rm -rf "$tmpdir"' EXIT HUP INT TERM
 
-## ipset list
-echo "create icloudrelay hash:net" > ip-ranges.ipset
-for ip in $(cat ip-ranges.txt); do
-  echo "add icloudrelay $ip" >> ip-ranges.ipset
-done
+write_ipset() {
+  input_file=$1
+  output_file=$2
+  set_name=$3
 
-echo "create icloudrelayipv4 hash:net" > ipv4/ipv4-ranges.ipset
-for ip in $(cat ipv4/ipv4-ranges.txt); do
-  echo "add icloudrelayipv4 $ip" >> ipv4/ipv4-ranges.ipset
-done
+  {
+    printf 'create %s hash:net\n' "$set_name"
+    while IFS= read -r ip; do
+      [ -n "$ip" ] || continue
+      printf 'add %s %s\n' "$set_name" "$ip"
+    done < "$input_file"
+  } > "$output_file"
+}
 
-echo "create icloudrelayipv6 hash:net" > ipv6/ipv6-ranges.ipset
-for ip in $(cat ipv6/ipv6-ranges.txt); do
-  echo "add icloudrelayipv6 $ip" >> ipv6/ipv6-ranges.ipset
-done
+write_json() {
+  input_file=$1
+  output_file=$2
 
-## json files
-cat ip-ranges.txt | jq -R --slurp 'split("\n") | .[:-1]' > ip-ranges.json
-cat ipv4/ipv4-ranges.txt | jq -R --slurp 'split("\n") | .[:-1]' > ipv4/ipv4-ranges.json
-cat ipv6/ipv6-ranges.txt | jq -R --slurp 'split("\n") | .[:-1]' > ipv6/ipv6-ranges.json
+  jq -Rcs 'split("\n") | map(select(length > 0))' < "$input_file" > "$output_file"
+}
+
+fetch_ranges() {
+  curl -fsSL --retry 3 --retry-delay 2 --retry-connrefused "$feed_url" \
+    | cut -d ',' -f 1 > "$tmpdir/egress-ip-ranges.txt"
+
+  [ -s "$tmpdir/egress-ip-ranges.txt" ]
+
+  grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+(/[0-9]+)?$' "$tmpdir/egress-ip-ranges.txt" > "$tmpdir/ipv4-only.txt"
+  grep -E '^[0-9a-fA-F:]+(/[0-9]+)?$' "$tmpdir/egress-ip-ranges.txt" > "$tmpdir/ipv6-only.txt"
+
+  total_count=$(wc -l < "$tmpdir/egress-ip-ranges.txt")
+  ipv4_count=$(wc -l < "$tmpdir/ipv4-only.txt")
+  ipv6_count=$(wc -l < "$tmpdir/ipv6-only.txt")
+
+  [ "$total_count" -gt 0 ]
+  [ $((ipv4_count + ipv6_count)) -eq "$total_count" ]
+
+  printf '%s %s\n' "$total_count" "$tmpdir/egress-ip-ranges.txt"
+  printf '%s %s\n' "$ipv4_count" "$tmpdir/ipv4-only.txt"
+  printf '%s %s\n' "$ipv6_count" "$tmpdir/ipv6-only.txt"
+}
+
+build_ranges() {
+  cidr-merger -eo "$tmpdir/ip-ranges.txt" "$tmpdir/egress-ip-ranges.txt"
+  cidr-merger -eo "$tmpdir/ipv4-ranges.txt" "$tmpdir/ipv4-only.txt"
+  cidr-merger -eo "$tmpdir/ipv6-ranges.txt" "$tmpdir/ipv6-only.txt"
+
+  wc -l "$tmpdir/ip-ranges.txt"
+  wc -l "$tmpdir/ipv4-ranges.txt"
+  wc -l "$tmpdir/ipv6-ranges.txt"
+}
+
+publish_outputs() {
+  write_ipset "$tmpdir/ip-ranges.txt" "$tmpdir/ip-ranges.ipset" "icloudrelay"
+  write_ipset "$tmpdir/ipv4-ranges.txt" "$tmpdir/ipv4-ranges.ipset" "icloudrelayipv4"
+  write_ipset "$tmpdir/ipv6-ranges.txt" "$tmpdir/ipv6-ranges.ipset" "icloudrelayipv6"
+
+  write_json "$tmpdir/ip-ranges.txt" "$tmpdir/ip-ranges.json"
+  write_json "$tmpdir/ipv4-ranges.txt" "$tmpdir/ipv4-ranges.json"
+  write_json "$tmpdir/ipv6-ranges.txt" "$tmpdir/ipv6-ranges.json"
+
+  mv "$tmpdir/egress-ip-ranges.txt" egress-ip-ranges.txt
+  mv "$tmpdir/ipv4-only.txt" ipv4-only.txt
+  mv "$tmpdir/ipv6-only.txt" ipv6-only.txt
+  mv "$tmpdir/ip-ranges.txt" ip-ranges.txt
+  mv "$tmpdir/ip-ranges.ipset" ip-ranges.ipset
+  mv "$tmpdir/ip-ranges.json" ip-ranges.json
+  mv "$tmpdir/ipv4-ranges.txt" ipv4/ipv4-ranges.txt
+  mv "$tmpdir/ipv4-ranges.ipset" ipv4/ipv4-ranges.ipset
+  mv "$tmpdir/ipv4-ranges.json" ipv4/ipv4-ranges.json
+  mv "$tmpdir/ipv6-ranges.txt" ipv6/ipv6-ranges.txt
+  mv "$tmpdir/ipv6-ranges.ipset" ipv6/ipv6-ranges.ipset
+  mv "$tmpdir/ipv6-ranges.json" ipv6/ipv6-ranges.json
+}
+
+fetch_ranges
+build_ranges
+publish_outputs
